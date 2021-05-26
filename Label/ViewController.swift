ae89f4a73712097bd0e1eb1ca02a9cf531d2a0eb
Taalai <taalai.z01@gmail.com>
2021-05-26 09:22:39 +0600

    Initial Commit

---
+++ Label/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Label
+//
+//  Created by Amazing on 5/26/21.
+//  Copyright © 2021 Amazing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
