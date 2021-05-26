f9e5d419c72d83ed75cf84f0a9db1ac34c460b61
Taalai <taalai.z01@gmail.com>
2021-05-26 10:55:08 +0600

    First push

---
+++ Label/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Label
+//
+//  Created by Amazing on 5/26/21.
+//  Copyright © 2021 Amazing. All rights reserved.
+//
+
--- Label/ViewController.swift
+++ Label/ViewController.swift
@@ -9,11 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    
+    var count = 0
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        view.backgroundColor = UIColor.red
+        
+        label.alpha = 0.5
+        
+        
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        
+        counter()
+        
+        plusplus()
+    }
+    
+    
+    func counter () {
+        if count % 2 == 0 {
+            label.text = "2"
+        }else {
+            label.text = "1"
+        }
+        
+      
+        
+    }
+    
+    func plusplus() {
+        count += 1
+    }
+    
 
 
 }
